eb1af09ac53b2dfd20aa96fc4aca39b73d48ee51
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-09-08 03:49:14 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -2,6 +2,7 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'home_page_model.dart';
@@ -51,11 +52,77 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                 children: [
                   Row(
                     mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Icon(
                         Icons.menu,
-                        color: FlutterFlowTheme.of(context).secondaryText,
+                        color: Colors.black,
                         size: 24.0,
+                      ),
+                      Align(
+                        alignment: AlignmentDirectional(0.00, 0.00),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            FaIcon(
+                              FontAwesomeIcons.eyeSlash,
+                              color: Colors.black,
+                              size: 24.0,
+                            ),
+                            Icon(
+                              Icons.qr_code_scanner_rounded,
+                              color: Colors.black,
+                              size: 24.0,
+                            ),
+                            Icon(
+                              Icons.notifications_outlined,
+                              color: Colors.black,
+                              size: 24.0,
+                            ),
+                          ].divide(SizedBox(width: 8.0)),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Column(
+                        mainAxisSize: MainAxisSize.max,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Olá, Bruno',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontFamily: 'SF Pro Display',
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.0,
+                            ),
+                          ),
+                          Text(
+                            'Saldo atual',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontFamily: 'SF Pro Display',
+                              color: Color(0xFF595F67),
+                              fontWeight: FontWeight.normal,
+                              fontSize: 12.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        'R\$4.590,00',
+                        style: TextStyle(
+                          fontFamily: 'SF Pro Display',
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          height: 19.0,
+                        ),
                       ),
                     ],
                   ),
